ea921fcc00b771bbfc441e7abe58c63f92780b47
Jérémy Goupil <goupiljeremy@gmail.com>
2021-11-29 16:27:56 +0100

    render the gift list (#9)

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -7,3 +7,13 @@ const app = shallow(<App />);
 it('renders correctly', () => {
     expect(app).toMatchSnapshot();
 });
+
+it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('add a new gift to `state` when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.state().gifts).toEqual([{ id: 1}]);
+});
