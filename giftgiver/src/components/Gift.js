b6b7e1cce06d0cb6429738324348c4e124818588
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-06 22:29:44 +0100

    Remove a Gift: issue #14

--- giftgiver/src/components/Gift.js
+++ giftgiver/src/components/Gift.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, FormControl, FormLabel } from 'react-bootstrap'; 
+import { 
+    Form, 
+    FormGroup, 
+    FormControl,
+    FormLabel, 
+    Button,
+} from 'react-bootstrap'; 
 
 class Gift extends Component {
     constructor () {
@@ -27,6 +33,12 @@ class Gift extends Component {
                         />
                     </FormGroup>
                 </Form>
+                <Button 
+                    className='btn-remove'
+                    onClick={() => this.props.removeGift(this.props.gift.id)}
+                >
+                Remove Gift
+                </Button> 
             </div>
         )
     };
