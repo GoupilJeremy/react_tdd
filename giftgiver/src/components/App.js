0b57dad9af976b4f8baf7cdf9bce09cceb83befe
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-18 09:41:22 +0100

    Test coverage and helper methods Fixes #15

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Gift from './Gift';
+import { max_number } from '../helper';
 
 class App extends Component {
     constructor() {
@@ -12,11 +13,7 @@ class App extends Component {
     addGift = () => {
         const { gifts } = this.state;
 
-        const ids = this.state.gifts.map(gift => gift.id);
-        
-        const max_id = ids.length > 0 ? Math.max(...ids) : 0;
-
-        gifts.push({ id: max_id + 1});
+        gifts.push({ id: max_number(this.state.gifts.map(gift => gift.id) + 1)});
 
         this.setState({ gifts });
     }
