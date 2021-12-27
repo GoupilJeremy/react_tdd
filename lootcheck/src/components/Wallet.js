cce7e2cd3b90a9c8b6b0692ae9dc3691c023f245
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-27 16:47:22 +0100

    local wallet balance with state #26 (#35)

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+    constructor(){
+        super();
+
+        this.state = { balance: undefined };
+    }
+
+    updateBalance = event =>  this.setState({ balance: parseInt(event.target.value) });
+
     render() {
         return (
             <div>
                 <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
+                <br />
+                <input className='input-wallet' onChange={this.updateBalance} />
             </div>
         )
     }
