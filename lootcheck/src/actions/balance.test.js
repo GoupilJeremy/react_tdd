0d7adb486377a575fab6620faa715ed6f0df7eb6
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-27 16:30:14 +0100

    support redux deposit and withdraw methods #25

--- lootcheck/src/actions/balance.test.js
+++ lootcheck/src/actions/balance.test.js
@@ -8,3 +8,19 @@ it('creates an action to set the balance', () => {
 
     expect(actions.setBalance(balance)).toEqual(expectedAction);
 });
+
+it('creates an action to deposit into the balance', () => {
+    const deposit = 10;
+
+    const expectedAction = { type: constants.DEPOSIT, deposit};
+
+    expect(actions.deposit(deposit)).toEqual(expectedAction);
+});
+
+it('creates an action to withdraw from the balance', () => {
+    const withdrawal = 10;
+
+    const expectedAction = { type: constants.WITHDRAW, withdrawal };
+
+    expect(actions.withdraw(withdrawal)).toEqual(expectedAction);
+});
