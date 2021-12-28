270f3b7c4ff021a40b3073d88f974dfbd26da7c4
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-28 18:26:51 +0100

    Styling and API cleanup #47

--- lootcheck/src/index.js
+++ lootcheck/src/index.js
@@ -5,6 +5,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import App from './components/App';
+import './index.css';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
