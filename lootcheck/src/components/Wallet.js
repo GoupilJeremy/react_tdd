c46055be659931fff2f70df21a008f5b4f752bf4
Jérémy Goupil <goupiljeremy@gmail.com>
2021-12-28 15:45:06 +0100

    Combine Reducers #42 (#51)

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -32,4 +32,4 @@ export class Wallet extends Component {
     }
 }
 
-export default connect(state => { return { balance: state } }, { deposit, withdraw })(Wallet);
+export default connect(state => { return { balance: state.balance } }, { deposit, withdraw })(Wallet);
